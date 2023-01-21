40229bdb31149188b64f11f49a36f055570708c7
Santiago Duque <santique227@gmail.com>
2023-01-20 20:32:34 -0500

    restore files

---
+++ backend/relaxandenjoy/src/main/java/com/pi/relaxandenjoy/RelaxandenjoyApplication.java
@@ -0,0 +1,13 @@
+package com.pi.relaxandenjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxandenjoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxandenjoyApplication.class, args);
+	}
+
+}
