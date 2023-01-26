ea3aabf49a2deb72b811d4bacde1cac8df304d51
LuisM11 <luisfeli1097@gmail.com>
2023-01-26 07:32:49 -0500

    Renaming frontend's root folder

---
+++ frontend/relaxandenjoy/src/components/DoubleCalendar.jsx
@@ -0,0 +1,16 @@
+
+
+
+const DoubleCalendar = () => {
+
+    return(
+        <input
+            type="date"
+            name=""
+            id=""
+            className="h-9 rounded shadow-2xl p-1"
+          />
+    )
+
+
+}
