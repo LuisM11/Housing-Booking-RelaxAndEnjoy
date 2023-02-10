fcd1d486e58b862852e10223921e97d56973a334
LuisM11 <luisfeli1097@gmail.com>
2023-02-10 16:19:56 -0500

    Improving cards UI

--- frontend/relaxandenjoy/src/components/ProductCard.jsx
+++ frontend/relaxandenjoy/src/components/ProductCard.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 function ProductCard({ item }) {
   return (
-    <article className="w-full h-128 tablet:h-64 bg-fourthColor rounded-lg shadow-md grid grid-rows-2 tablet:grid-cols-2 tablet:py-2 tablet:gap-2">
+    <article className="w-full h-128 tablet:h-64 bg-fourthColor rounded-lg shadow-md grid grid-rows-2 tablet:grid-cols-2  tablet:gap-2">
+     <figure className="h-64 w-full">
       <img
         src={item.crimg}
         alt={item.name}
         title={item.name}
-        className="h-60 w-full rounded-b-lg tablet:rounded-l-none tablet:rounded-r-lg"
-      />
-      <div className="w-full h-64 flex flex-col p-2">
+        className=" object-cover rounded-t-lg  w-full h-full tablet:rounded-none tablet:rounded-l-lg "
+        />
+      </figure>
+      <div className="w-full h-64 flex flex-col py-5 px-4 desktop:p-2">
         <div className="h-13 flex justify-between">
           <aside>
             <span className="text-xs text-secundaryColor opacity-50">
@@ -60,8 +62,8 @@ function ProductCard({ item }) {
             </h2>
           </aside>
           <aside className="flex flex-col justify-center items-end">
-            <p className="w-8 text-center text-fourthColor text-lg bg-secundaryColor rounded-lg">
-              {item.popularity}
+            <p className="px-1 py-0.5 text-center text-fourthColor text-base bg-secundaryColor rounded-lg">
+              {item.popularity.toFixed(1)}
             </p>
             <p className="text-sm text-right text-secundaryColor">
               {item.popularity < 3
@@ -76,23 +78,23 @@ function ProductCard({ item }) {
           <div>
             <i className="uil uil-map-marker"></i> 
           <span className="text-secundaryColor text-sm font-medium max-w-[48%] h-fit pl-1">
-            {item.location}
+            {item.location.split(",")[0]}
           </span>
           </div>
           <a href="" className="text-mainColor text-sm">
             MOSTRAR EN EL MAPA
           </a>
         </div>
-        <div className="flex items-center justify-around h-14">
+        <div className="flex items-center justify-start h-14">
           {item.features.map((ft, index) => {
           return (
-              <i className={`${ft.icon} text-xl text-secundaryColor`}></i>          
+              <i className={`${ft.icon} text-lg text-secundaryColor mr-2`}></i>          
           );
         })}
         </div>
         <div className="h-20 font-medium text-[13px] flex items-end pb-2">
           <p>
-          {item.description.slice(0,70)+ '... '} 
+          {item.description.slice(0,80)+ '... '} 
           <Link className="text-mainColor" to={`/Product/${item.id}`}> más</Link>
           </p>
           </div>
